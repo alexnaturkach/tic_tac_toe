e019ac78e7c2305114608ed2859c4198ec103276
Oleksandr Naturkach <oleksandrnaturkach@gmail.com>
2020-10-17 09:38:55 +0300

    Unfinished minimax

--- script.js
+++ script.js
@@ -14,7 +14,57 @@ const gameboard = (() => {
   }
 
 
-  const checkForWinner = () => {
+  // const checkForWinner = () => {
+  //   const winConditions = [
+  //     //rows
+  //     [0, 1, 2],
+  //     [3, 4, 5],
+  //     [6, 7, 8],
+  //     //columns
+  //     [0, 3, 6],
+  //     [1, 4, 7],
+  //     [2, 5, 8],
+  //     //diagonals
+  //     [0, 4, 8],
+  //     [2, 4, 6]
+  //   ];
+  //
+  //   let x_OnBoard = [];
+  //   let o_OnBoard = [];
+  //   //sort X and O indices to corresponding arrays
+  //   for (i = 0; i < gameBoard.length; i++) {
+  //     if (gameBoard[i] === player1.getWeapon()) {
+  //       x_OnBoard.push(i);
+  //     } else if (gameBoard[i] === player2.getWeapon()) {
+  //       o_OnBoard.push(i);
+  //     }
+  //   }
+  //
+  //   const checker = (arr, winCon) => {
+  //     for (i = 0; i < winCon.length; i++) {
+  //       if (winCon[i].every(item => arr.includes(item))) {
+  //
+  //         winCon[i].forEach(element => boardNodes[element].style.filter = "invert(1) brightness(50%) sepia(100%) saturate(2005%) hue-rotate(120deg)");
+  //         game.getTurn().incrementScore();
+  //         game.setWinner();
+  //         game.announceWinner();
+  //         game.stopGame();
+  //         game.updateScore();
+  //         x_OnBoard = [];
+  //         o_OnBoard = [];
+  //         return;
+  //       } else if (!(winCon[i].every(item => arr.includes(item))) & !gameBoard.includes(0)) {
+  //         game.announceDraw();
+  //         game.announceWinner();
+  //       }
+  //     }
+  //   }
+  //   checker(o_OnBoard, winConditions);
+  //   checker(x_OnBoard, winConditions);
+  // }
+
+  const checkWinner = () => {
+    let winner = null;
     const winConditions = [
       //rows
       [0, 1, 2],
@@ -40,44 +90,100 @@ const gameboard = (() => {
       }
     }
 
-    const checker = (arr, winCon) => {
+    const checker = (arr, arr2, winCon) => {
       for (i = 0; i < winCon.length; i++) {
         if (winCon[i].every(item => arr.includes(item))) {
-          console.log(`The winner is ${game.getTurn().getName()}`);
-          winCon[i].forEach(element => boardNodes[element].style.filter = "invert(1) brightness(50%) sepia(100%) saturate(2005%) hue-rotate(120deg)");
-          game.getTurn().incrementScore();
-          game.setWinner();
-          game.announceWinner();
-          game.stopGame();
-          game.updateScore();
-          x_OnBoard = [];
-          o_OnBoard = [];
-        } else if (!(winCon[i].every(item => arr.includes(item))) & !gameBoard.includes(0)) {
-          game.announceDraw();
-          game.announceWinner();
-          x_OnBoard = [];
-          o_OnBoard = [];
-          return;
+
+          winner = player1.getWeapon();
+        } else if (winCon[i].every(item => arr2.includes(item))) {
+          winner = player2.getWeapon();
         }
       }
     }
-    checker(x_OnBoard, winConditions);
-    checker(o_OnBoard, winConditions);
-
+    checker(x_OnBoard, o_OnBoard, winConditions);
+    if (winner !== null) {
+      return winner;
+    } else if (!gameBoard.includes(0) & winner === null) {
+      return 'tie';
+    } else {
+      return winner;
+    }
   }
+
+  const emptyCells = gameBoard.filter(cell => cell === 0);
+
+  function bestMove() {
+    // AI to make its turn
+    let bestScore = -Infinity;
+    let bestMove;
+    for (i = 0; i < gameBoard.length; i++) {
+      if (gameBoard[i] === 0) {
+        gameBoard[i] = player2.getWeapon();
+        let score = minimax(gameBoard, 0, false);
+        gameBoard[i] = 0;
+        if (score > bestScore) {
+          bestScore = score;
+          bestMove = i;
+        }
+      }
+    }
+    console.log(bestMove);
+    gameBoard[bestMove] = player2.getWeapon();
+    game.setTurn(player1);
+    boardNodes[bestMove].classList.add(player2.getWeapon());
+  }
+
+  let scores = {
+    x: -10,
+    o: 10,
+    tie: 0
+  };
+
+  function minimax(board, depth, isMaximizing) {
+    let result = checkWinner();
+    if (result !== null) {
+      return scores[result];
+    }
+
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (i = 0; i < gameBoard.length; i++) {
+        if (gameBoard[i] === 0) {
+          gameBoard[i] = player2.getWeapon();
+          let score = minimax(gameBoard, depth + 1, false);
+          gameBoard[i] = 0;
+          bestScore = Math.max(score, bestScore);
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (i = 0; i < gameBoard.length; i++) {
+        if (gameBoard[i] === 0) {
+          gameBoard[i] = player1.getWeapon();
+          let score = minimax(gameBoard, depth + 1, true);
+          gameBoard[i] = 0;
+          bestScore = Math.min(score, bestScore);
+        }
+      }
+      return bestScore;
+    }
+  }
+
 
   const move = (target) => {
     if (gameBoard[target] === 0 & !game.isGameStopped()) {
       gameBoard[target] = game.getTurn().getWeapon();
       boardNodes[target].classList.add(game.getTurn().getWeapon());
-      console.log(gameBoard);
-      checkForWinner();
+      // checkForWinner();
       if (game.getTurn() === player1) {
         game.setTurn(player2);
       } else {
         game.setTurn(player1);
       }
     }
+    bestMove();
+    console.log(checkWinner());
   }
 
   const resetBoard = () => {
@@ -88,12 +194,12 @@ const gameboard = (() => {
       }
       boardNodes[i].style.filter = "none";
     }
-    console.log(gameBoard);
+
   }
   return {
     move,
     resetBoard,
-    checkForWinner,
+    // checkForWinner,
   };
 })();
 
@@ -125,14 +231,19 @@ const player1 = player('p1', 'x');
 const player2 = player('p2', 'o');
 
 //rename players for PVP
-function createPlayers(){
+function createPlayers() {
   const p1 = document.getElementById('p1_name').value;
   const p2 = document.getElementById('p2_name').value;
-player1.setName(p1);
-player2.setName(p2);
-document.getElementById('overlay').style.display = "none";
-document.getElementById('p1name').innerText = player1.getName();
-document.getElementById('p2name').innerText = player2.getName();
+  if (p1 === '' & p2 === '') {
+    player1.setName('Player 1');
+    player2.setName('Player 2');
+  } else {
+    player1.setName(p1);
+    player2.setName(p2);
+  }
+  game.resetGameScore();
+  document.getElementById('p1name').innerText = player1.getName();
+  document.getElementById('p2name').innerText = player2.getName();
 }
 
 
@@ -148,11 +259,20 @@ const game = (() => {
   const getTurn = () => playerTurn;
   const setTurn = (player) => {
     playerTurn = player;
-    console.log(`It's ${playerTurn.getName()}'s turn`);
+
   }
+
   const updateScore = () => {
     score1.innerText = player1.getScore();
     score2.innerText = player2.getScore();
+  }
+  const resetGameScore = () => {
+    document.getElementById('burger').classList.toggle('is-active');
+    player1.resetScore();
+    player2.resetScore();
+    toggleOverlay();
+    updateScore();
+    newRound();
   }
   const stopGame = () => gameStop = true;
   const continueGame = () => gameStop = false;
@@ -162,6 +282,7 @@ const game = (() => {
   };
   const announceWinner = () => {
     announcer.style.display = "block";
+    document.getElementById('resetBoardButton').style.visibility = 'hidden';
   }
   const clearWinner = () => {
     winner.innerText = ""
@@ -179,11 +300,13 @@ const game = (() => {
     continueGame();
     clearWinner();
     clearAnnouncer();
+    document.getElementById('resetBoardButton').style.visibility = 'visible';
   }
   return {
     getTurn,
     setTurn,
     updateScore,
+    resetGameScore,
     setWinner,
     announceWinner,
     announceDraw,
@@ -199,5 +322,23 @@ const game = (() => {
 
 //events
 function showNameInputs() {
-document.querySelector('#namesForm').classList.toggle('active');
+  document.querySelector('#namesForm').classList.toggle('active');
 }
+
+function toggleOverlay() {
+  document.getElementById('overlay').classList.toggle('hidden');
+}
+
+
+document.getElementById('resetBoardButton').addEventListener('mousedown', (e) => {
+  e.target.classList.add('clickedReset');
+})
+
+document.addEventListener('mouseup', (e) => {
+  if (e.target === document.getElementById('resetBoardButton')) {
+    gameboard.resetBoard();
+    e.target.classList.remove('clickedReset');
+  } else {
+    document.getElementById('resetBoardButton').classList.remove('clickedReset');
+  }
+})
